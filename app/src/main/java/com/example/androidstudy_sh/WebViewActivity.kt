81a1e1a1b3385ea38b67ebc35adcb1faed51620c
ssanay88 <pplloo748@gmail.com>
2022-08-17 08:08:41 +0900

    WebView 설정

--- app/src/main/java/com/example/androidstudy_sh/WebViewActivity.kt
+++ app/src/main/java/com/example/androidstudy_sh/WebViewActivity.kt
@@ -1,17 +1,32 @@
 package com.example.androidstudy_sh
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.widget.Toast
 
 class WebViewActivity : AppCompatActivity() {
 
-    val myWebView: WebView = findViewById(R.id.webView)
-    
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_web_view)
+
+        val myWebView: WebView = findViewById(R.id.webView)
+        myWebView.loadUrl("https://AndroidStudy.pplloo748.repl.co")
+        myWebView.settings.javaScriptEnabled = true
+
     }
+}
+
+class WebAppInterface(private val mContext: Context) {
+
+    @JavascriptInterface
+    fun showToast() {
+        Toast.makeText(mContext, "성공적으로 등록되었습니다.", Toast.LENGTH_SHORT).show()
+    }
+
 }
